ae009916077e3973b5be5e5c8a4be6eae7ea3100
karn09 <karn09@gmail.com>
2015-03-05 21:18:19 -0500

    created IFFE within loop to track individual clicks on links

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-var clicked = document.getElementById('kittyclick');
-var clicked2 = document.getElementById('kittyclick2');
+var catsArray = document.getElementsByTagName('li')
 
-
-var insertClicks = document.getElementById('clickTime');
-var insertClicks2 = document.getElementById('clickTime2');
-
-var clickListener = function(clickElement, counterElement) {
+/*var clickListener = function(clickElement, counterElement) {
     var timesClicked = 0
     clickElement.addEventListener('click', function() {
         timesClicked++
         counterElement.innerHTML = timesClicked;
-        if (timesClicked > 20) {
-            wtf()
-        }
     })
 }
-
-clickListener(clicked,insertClicks)
-clickListener(clicked2,insertClicks2)
-
-
-function wtf() {
-    console.log('wtf')
+*/
+for (var i = 0; i < catsArray.length; i++) {
+	var cat = catsArray[i];
+	cat.addEventListener('click', (function(copyCat) {
+		var num = 0;
+		return function () {
+			num++;
+			console.log(copyCat.innerText + ": " +num);
+		}
+	})(cat))
 }
